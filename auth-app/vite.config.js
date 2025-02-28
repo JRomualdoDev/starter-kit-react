5feccb77cca57aa14b557f5b73e0dadb28dbc4d6
JRomualdoDev <jromualdo3@hotmail.com>
2025-02-27 23:41:13 -0300

    ajustes

--- auth-app/vite.config.js
+++ auth-app/vite.config.js
@@ -5,9 +5,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: ['resources/js/app.tsx'],
             refresh: true,
         }),
         react(),
     ],
+    server: {
+        host: '0.0.0.0',  // Garante que o Vite escute em todas as interfaces
+        port: 5173,
+        hmr: {
+            host: 'host.docker.internal',  // Define o host para hot reloading
+        },
+    },
 });
